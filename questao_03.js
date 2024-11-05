eb866e5da29c4fbc815a4953c17bd07d24215dc6
madud4 <160739525+madud4@users.noreply.github.com>
2024-11-05 18:52:24 -0300

    atividade 06

---
+++ questao_03.js
@@ -0,0 +1,12 @@
+let nome = "Maria Eduarda";
+let nota1 = 9;
+let nota2 = 8.7;
+
+let multi1 = nota1 * 0.6;
+let multi2 = nota2 * 0.4;
+let soma = multi1 + multi2;
+
+alert('Nome do aluno: ' + nome)
+alert('Nota 1: ' + nota1)
+alert('Nota 2: ' + nota2)
+alert('Nota total: ' + soma)
